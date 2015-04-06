eafe6d2bc1e092ace692af345529d3d676a3e646
Darya Dziubkina <dasha.dubkina@gmail.com>
2015-04-06 18:54:44 +0300

    Создан класс NoteDaoImpl наследующий интерфейы NoteDao.

---
+++ src/main/java/daoImpl/NotificationDaoImpl.java
@@ -0,0 +1,22 @@
+package daoImpl;
+
+
+import dao.NotificationDao;
+
+public class NotificationDaoImpl implements NotificationDao {
+    public void createNotification() {
+
+    }
+
+    public void changeNotification() {
+
+    }
+
+    public void deleteNotification() {
+
+    }
+
+    public void readingNotification() {
+
+    }
+}

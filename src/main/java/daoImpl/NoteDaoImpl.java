457848059f29b6bd6af2c4ef743b80a37bd59d5b
Darya Dziubkina <dasha.dubkina@gmail.com>
2015-04-06 18:57:47 +0300

    Создан класс NoteDao имплементирующий интерефейс NoteDao

---
+++ src/main/java/daoImpl/NoteDaoImpl.java
@@ -0,0 +1,22 @@
+package daoImpl;
+
+
+import dao.NoteDao;
+
+public class NoteDaoImpl implements NoteDao{
+    public void createNote() {
+
+    }
+
+    public void changeNote() {
+
+    }
+
+    public void deleteNote() {
+
+    }
+
+    public void readingNote() {
+
+    }
+}

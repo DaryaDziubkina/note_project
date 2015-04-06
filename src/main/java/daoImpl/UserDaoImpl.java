9005bcdf6b6f378704d3b7c62fb8647fb1163cd2
Darya Dziubkina <dasha.dubkina@gmail.com>
2015-04-06 18:56:36 +0300

    Создан класс UserDao имплементирующий интерфейс UserDao

---
+++ src/main/java/daoImpl/UserDaoImpl.java
@@ -0,0 +1,21 @@
+package daoImpl;
+
+import dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+    public void createUser() {
+
+    }
+
+    public void changeUser() {
+
+    }
+
+    public void deleteUser() {
+
+    }
+
+    public void readingUser() {
+
+    }
+}

544853ff5c00d1821e0eab861a6f925479ecf0f2
Darya Dziubkina <dasha.dubkina@gmail.com>
2015-04-08 14:58:18 +0300

    Класс перенесен в ощий пакет by.gsu.dashadubkina

--- src/main/java/by/gsu/dashadubkina/servlets/MainServlet.java
+++ src/main/java/by/gsu/dashadubkina/servlets/MainServlet.java
@@ -1,4 +1,4 @@
-package servlets;
+package by.gsu.dashadubkina.servlets;
 
 
 import javax.servlet.ServletException;
@@ -14,6 +14,8 @@ public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
+
+
     }
 
     @Override

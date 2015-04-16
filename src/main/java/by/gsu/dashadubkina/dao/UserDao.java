495a98240ea097a3403183ec035eed53b91f2a2c
Darya Dziubkina <dasha.dubkina@gmail.com>
2015-04-16 15:09:23 +0300

    Добавлен метод, изменены входные параметры и возращаемые значения

--- src/main/java/by/gsu/dashadubkina/dao/UserDao.java
+++ src/main/java/by/gsu/dashadubkina/dao/UserDao.java
@@ -3,15 +3,19 @@ package by.gsu.dashadubkina.dao;
 
 import by.gsu.dashadubkina.objects.User;
 
+import java.util.List;
+
 public interface UserDao {
 
     long createUser(User user);
 
-    void changeUser(User user);
+    User getUser(long id);
 
-    void deleteUser(User user);
+    void deleteUser(long id);
 
     void updateUser(User user);
+
+    List<User> getAll();
 
 
 }

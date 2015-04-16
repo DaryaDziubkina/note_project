c84a1e716eb1d862fcbb4bbbeb341dc856957533
Darya Dziubkina <dasha.dubkina@gmail.com>
2015-04-16 15:08:51 +0300

    Добавлены анноации, метод getSession(), описаны методы

--- src/main/java/by/gsu/dashadubkina/daoImpl/NotificationDaoImpl.java
+++ src/main/java/by/gsu/dashadubkina/daoImpl/NotificationDaoImpl.java
@@ -4,33 +4,51 @@ package by.gsu.dashadubkina.daoImpl;
 import by.gsu.dashadubkina.dao.NotificationDao;
 import by.gsu.dashadubkina.objects.Notification;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
+import java.util.List;
 
-@Repository
+@Repository("NotificationDao")
 public class NotificationDaoImpl implements NotificationDao {
+
     private Session session;
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public NotificationDaoImpl(){
+        super();
+    }
 
     public NotificationDaoImpl(Session session) {
         this.session = session;
     }
 
+    public Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
     public long createNotification(Notification notification) {
-        notification.setDateTime(new Date());
-        session.save(notification);
+        getSession().save(notification);
         return notification.getId();
     }
 
-    public void changeNotification(Notification notification) {
-
+    public Notification getNotification(long id) {
+        return (Notification) getSession().get(Notification.class, id);
     }
 
-    public void deleteNotification(Notification notification) {
+    public void deleteNotification(long id) {
+        Notification notification = (Notification) getSession().get(Notification.class, id);
+        getSession().delete(notification);
 
     }
 
     public void updateNotification(Notification notification) {
+        getSession().saveOrUpdate(notification);
+    }
 
+    public List<Notification> getAll() {
+        return getSession().createQuery("FROM Notification").list();
     }
 }

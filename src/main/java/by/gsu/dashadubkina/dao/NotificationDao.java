4ff90c2e9c1cdd4bf733a7a3c3f74efd4aed7809
Darya Dziubkina <dasha.dubkina@gmail.com>
2015-04-16 15:06:44 +0300

    Добавлен метод, изменены входные параметры и возращаемые значения

--- src/main/java/by/gsu/dashadubkina/dao/NotificationDao.java
+++ src/main/java/by/gsu/dashadubkina/dao/NotificationDao.java
@@ -3,11 +3,18 @@ package by.gsu.dashadubkina.dao;
 
 import by.gsu.dashadubkina.objects.Notification;
 
+import java.util.List;
+
 public interface NotificationDao {
 
     long createNotification(Notification notification);
-    void changeNotification(Notification notification);
-    void deleteNotification(Notification notification);
+
+    Notification getNotification(long id);
+
+    void deleteNotification(long id);
+
     void updateNotification(Notification notification);
+
+    List<Notification> getAll();
 
 }

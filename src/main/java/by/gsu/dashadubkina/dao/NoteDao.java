990284246f5410a64cbfa5d5b8aeb573b5e516d2
Darya Dziubkina <dasha.dubkina@gmail.com>
2015-04-16 15:00:28 +0300

    добавлен метод изменены входные параметры  и возвращаемые значения

--- src/main/java/by/gsu/dashadubkina/dao/NoteDao.java
+++ src/main/java/by/gsu/dashadubkina/dao/NoteDao.java
@@ -3,11 +3,14 @@ package by.gsu.dashadubkina.dao;
 
 import by.gsu.dashadubkina.objects.Note;
 
+import java.util.List;
+
 public interface NoteDao {
 
     long createNote(Note note);
-    void changeNote(Note note);
-    void deleteNote(Note note);
+    Note getNote(long id);
+    void deleteNote(long id);
     void updateNote(Note note);
+    List<Note> getAll();
 
 }
